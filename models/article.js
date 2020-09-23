542020bc7d601ff30e802994a3b2e07dadea09ac
YoshkinKoT-63 <yoshkinkot63@gmail.com>
2020-09-23 23:18:30 +0400

    added schem

--- models/article.js
+++ models/article.js
@@ -2,7 +2,50 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 
 const cardSchema = new mongoose.Schema({
-
+  keyword: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+  source: {
+    type: String,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+    validate: {
+      validator(valid) {
+        return validator.isURL(valid);
+      },
+      message: 'Неверный формат URL',
+    },
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator(valid) {
+        return validator.isURL(valid);
+      },
+      message: 'Неверный формат URL',
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+  },
 });
 
 module.exports = mongoose.model('article', cardSchema);
--- models/user.js
+++ models/user.js
@@ -10,22 +10,6 @@ const userSchema = new mongoose.Schema({
     maxlength: 30,
     required: true,
   },
-  about: {
-    type: String,
-    minlength: 2,
-    maxlength: 30,
-    required: true,
-  },
-  avatar: {
-    type: String,
-    required: true,
-    validate: {
-      validator(valid) {
-        return validator.isURL(valid);
-      },
-      message: 'Неверный формат URL',
-    },
-  },
   email: {
     type: String,
     required: true,
