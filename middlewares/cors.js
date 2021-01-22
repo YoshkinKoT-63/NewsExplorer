4dcd700159c7dbc8e4b90cb767ac9c1004b7c185
YoshkinKoT-63 <yoshkinkot63@gmail.com>
2021-01-23 01:17:25 +0400

    fix

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const cors = require('cors');
 
 const corsOptions = {
-  origin: ['http://localhost:8080', 'https://YoshkinKoT-63.github.io', 'https://newsxplorer.ru', 'http://newsxplorer.ru'],
+  origin: ['http://localhost:8080', 'https://YoshkinKoT-63.github.io', 'https://newsxplorer.ru', 'http://newsxplorer.ru', 'https://yoshkinkot-63.github.io/NewsExplorerFrontEnd/'],
   optionsSuccessStatus: 200,
   credentials: true,
 };
